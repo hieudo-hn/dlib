a622e6048745d618904d8933f20d6142e3873fa7
zbirenbaum <zacharyobirenbaum@gmail.com>
2021-08-11 16:14:14 -0400

    autopush

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-#syntax=docker/dockerfile:1
-FROM imglab:latest
-ENV DISPLAY :0
-WORKDIR /dlibSealGUI/run
-RUN ./imglab
--- rundocker.sh
+++
@@ -1,6 +0,0 @@
-docker run -it --rm \
-  --env=DISPLAY \
-  --env=QT_X11_NO_MITSHM=1 \
-  --volume=/tmp/.X11-unix:/tmp/.X11-unix:rw \
-  imglab:latest
-
