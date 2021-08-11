435b9d64d0094133023a5ed0d3d001894c9a42d2
zbirenbaum <zacharyobirenbaum@gmail.com>
2021-08-11 16:15:36 -0400

    "docker update"

---
+++ docker/initializedocker.sh
@@ -0,0 +1,5 @@
+docker run -it -e DISPLAY=host.docker.internal:0 \
+  --mount type=bind,source="$(pwd)"../,target=/dlibSealGUI \
+  --env="_X11_NO_MITSHM=1" \
+  --volume=/tmp/.X11-unix:/tmp/.X11-unix:rw \
+  imglab:latest /bin/bash
